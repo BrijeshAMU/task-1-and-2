bbafd6a010ccc9643e1d35b81412a05eedf40af2
BrijeshAMU <74918828+BrijeshAMU@users.noreply.github.com>
2020-11-29 01:00:20 +0530

    Project
    Basic calculation 1 and 2

---
+++ Basic calculation Task 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a;
+	float b;
+	cout<<"Enter two numbers:\n"<<endl;
+	cin>>a;
+    cin>>b;
+    cout<<"Product"<<a*b<<endl;
+    return 0;
+	
+}
